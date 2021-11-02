8e28a9a9a52ed5f5d7798aa4c5d63545f6342321
LauraFernandez <laura.fernandezmol@iesdoctorbalmis.com>
2021-11-02 15:06:24 +0100

    eje4r544

---
+++ MemeMaker/conversores/ActivarBorderConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class ActivarBorderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Me guardo el valor del isChecked, dependiendo si esta marcado o no devuelvo un grosor 2 o un grosor 0 //
+            bool estamarcado = (bool)value;
+            if (estamarcado)
+            {
+                return 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
